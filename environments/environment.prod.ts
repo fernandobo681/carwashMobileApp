a82d105ef371fb2d7f0fd670d959642ed891306f
fernando_bmx <fernandobo681@gmail.com>
2023-05-09 22:20:54 -0600

    estructura de archivos -@fernando_bmx

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    //SERVICES_URL: 'http://192.168.0.197:3000/api/', //API local en red
+    SERVICES_URL: 'https://api-dev.test.app/api/', //API Development
+    //SERVICES_URL: 'https://api.test.app/api/',// <-- usar cuando se genere un nuevo bundle de producción
+    production: true,
+  };
+  
