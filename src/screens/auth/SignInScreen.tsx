3393b63a7a788b8fe643b7817f2445c5035ac151
fernando_bmx <fernandobo681@gmail.com>
2023-06-07 00:42:28 -0600

    version estable y limpia

--- src/screens/auth/SignInScreen.tsx
+++ src/screens/auth/SignInScreen.tsx
@@ -1,63 +1,10 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Alert, StyleSheet, Text, FlatList, StatusBar } from 'react-native';
-import axios from 'axios';
-interface User {
-  email: string;
-  password: string;
-}
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function SignInScreen() {
-  const [email, setEmail] = useState('fernando@gmail.com');
-  const [password, setPassword] = useState('123456');
-  const [userInfo, setUserInfo] = useState<User[]>([]);
-
-  const handleSignIn = async () => {
-    try {
-      const response = await axios.post('http://192.168.1.70:3001/api/customers/login', { email, password });
-      setUserInfo(response.data);
-      console.log(userInfo);
-    } catch (error) {
-      Alert.alert('Error', 'Failed to save data.');
-    }
-  };
-
   return (
-    <>
     <View>
-    <StatusBar barStyle="dark-content" backgroundColor="#ffffff"/>
-      <Text style={styles.mainText}>Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={text => setEmail(text)}
-        value={email}
-      />
-      <Text style={styles.mainText}>Contraseña</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Sign In" onPress={handleSignIn} />
+      <Text>SignInScreen</Text>
     </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  mainText: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 20,
-    textAlign: 'justify'
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    color: 'black'
-  }
-});
+  )
+}
